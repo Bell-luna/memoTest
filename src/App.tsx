9b31cf42a49556e6b920da5953c6aed2c2d1de19
Belen Luna <belen.luna@flat.mx>
2023-10-20 12:48:05 -0300

    Agregando opciones de ruta

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Memotest from "./Memotest";
 import WordPerMinute from "./WordPerMinute";
 
 function App() {
   return (
     <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/memotest">Memotest</Link>
+          </li>
+          <li>
+            <Link to="/wpm">Word Per Minute</Link>
+          </li>
+        </ul>
+      </nav>
+
       <Routes>
         <Route path="/memotest" element={<Memotest />} />
         <Route path="/wpm" element={<WordPerMinute />} />
